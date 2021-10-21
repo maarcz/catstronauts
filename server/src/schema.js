92531363534a8df1f62726194e80718f8b405146
Marcis Zoldners <marcis.zoldners@gmail.com>
2021-10-21 21:04:42 +0300

    first commit

---
+++ server/src/schema.js
@@ -0,0 +1,60 @@
+const {gql} = require('apollo-server');
+
+const typeDefs = gql`
+    type Query {
+        "Get tracks array for homepage grid"
+        tracksForHome: [Track!]!
+        track(id: ID!): Track
+    }
+
+    type Mutation {
+        incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+    }
+
+    type IncrementTrackViewsResponse {
+        code: Int!
+        success: Boolean!
+        message: String!
+        track: Track
+    }
+    
+    "A track is a group of Modules that teaches about a specific topic"
+    type Track {
+        id: ID!
+        "The track's title"
+        title: String!
+        "The track's main author"
+        author: Author!
+        "The track's main illustration to display in track card or track page detail"
+        thumbnail: String
+        "The track's approximate length to complete, in minutes"
+        length: Int
+        "The number of modules this track contains"
+        modulesCount: Int
+        "The track's complete description, can be in Markdown format"
+        description: String
+        "The number of times a track has been viewed"
+        numberOfViews: Int
+        modules: [Module!]!
+    }
+
+    "A Module is a single unit of teaching. Multiple Modules compose a Track"
+    type Module {
+        id: ID!
+        "The Module's title"
+        title: String!
+        "The Module's length in minutes"
+        length: Int
+    }
+
+    "Author of a complete Track"
+    type Author {
+        id: ID!
+        "Author's first and last name"
+        name: String!
+        "Author's profile picture url"
+        photo: String
+    }
+`;
+
+  module.exports = typeDefs;
